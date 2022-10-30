6b608ede9efb56eb18a049400ff22869c16f5777
sriram <116995228+srir4mr@users.noreply.github.com>
2022-10-30 14:19:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 #python atm simulation
 #pin 0000
-bal==0
-print(/t/t/t"Welcome to GIT bank"\n);
-print(\t\t"Please insert your "\n);
-pin=int(input(\t"enter your pin"\n);
-While pin=="0000" :
- print("Choose Your Option "\n);
- opt=input("B->Balance Enquiry"/n"W->Withdrawl"/n"D->Deposit"/n");
-if opt==b :
- print("Your current account balance is:",bal\n);
-if opt==w :
- wid=int(input("Enter the withdraw amount"\n));
- if wid>bal :
-  print("insufficient fund"\n);
- else:
-  bal=bal-wid
-  print("Please collect your cash"\n);
-if opt==d :
- dep=int(input("enter the deposit amount"\n);
- bal=bal+dep
+
+balance=0
+
+print("\t\t\t Welcome to GIT bank\n");
+print("\t\tPlease insert your CARD \n");
+pin=int(input("\tenter your pin\n"));
+while pin==0000 :
+    print("Choose Your Option \n");
+    opt=input("B->Balance Enquiry\nW->Withdrawl\nD->Deposit\nE->EXIT\n");
+    if opt=="b" :
+        print("Your current account balance is:",balance);
+    if opt=="w" :
+        wid=int(input("Enter the withdraw amount\n"));
+        if wid>balance :
+            print("insufficient fund\n");
+        else:
+            balance=balance-wid
+        print("Please collect your cash\n");
+    if opt=="d" :
+        dep=int(input("enter the deposit amount\n"));
+        balance=balance+dep
+        print("Sucessfully deposited\n")
+    if opt=="e":
+        print("Thank you for banking with US :)\n")
+        break 
 else:
-print("Invalid pin);
+    print("Invalid pin");
